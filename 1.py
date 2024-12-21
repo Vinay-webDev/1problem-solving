fe656fc163445c8cdbe2b1b98c5d9714a2c18b28
Vinay-webDev <justchatting3000@gmail.com>
2024-12-22 03:46:09 +0530

    #1

---
+++ 1.py
@@ -0,0 +1,17 @@
+"""217. Contains Duplicates"""
+nums1 = [1, 2, 2, 3, 4]
+nums2 = [100, 100, 294, 393, 9399030, 838]
+nums3 = [10, 20, 39, 93]
+class Solution:
+    def containsDuplicates(self, nums):
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] == nums[j]:
+                    return True
+        return False
+sol = Solution()
+print(sol.containsDuplicates(nums1))  #True
+print(sol.containsDuplicates(nums2))  #True
+print(sol.containsDuplicates(nums3))  #False
+
+
