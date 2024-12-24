18ef72f43d0723c816b7dbf72725bf00aad79264
Vinay-webDev <justchatting3000@gmail.com>
2024-12-24 17:00:46 +0530

    #2

--- 1.py
+++ 1.py
@@ -40,22 +40,6 @@ sol = Solution()
 # print(sol.containsDuplicates(nums2))  #True
 # print(sol.containsDuplicates(nums3))  #False
 
-"""268.Missing Number"""
-nums1 = [3, 0, 1]
-nums2 = [2, 4, 5, 6, 8, 7, 9, 0, 1]
-nums3 = [0, 1]
-class Solution:
-    def missingNumber(self, nums):
-        nums.sort()
-        for index, value in enumerate(nums):
-            if index != value:
-                return index
-            if value == len(nums) - 1:
-                return value + 1
-sol = Solution()
-print(sol.missingNumber(nums1))     #2
-print(sol.missingNumber(nums2))     #3
-print(sol.missingNumber(nums3))     #2
 
 
 
---
+++ test.py
@@ -0,0 +1,9 @@
+"""enumerate"""
+fruits = ['apple', 'banana', 'orange', 'pineapple']
+for index, fruit in enumerate(fruits):
+    print(index, fruit)
+# 0 apple
+# 1 banana
+# 2 orange
+# 3 pineapple
+
