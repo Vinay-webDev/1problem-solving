a82d04da1bcaee5cdcae0dab0cd84786642861b8
Vinay-webDev <justchatting3000@gmail.com>
2024-12-25 23:17:03 +0530

    #5

--- 5.py
+++ 5.py
@@ -1,6 +1,5 @@
 """1365. How many Nubmers are smaller than the current number"""
-nums1 = [8,1,2,2,3]
-nums2 = [6,5,4,8]
+
 class Solution:
     def smallerNumbersThanCurrent(self, nums):
         res = []
@@ -12,9 +11,30 @@ class Solution:
             res.append(count)
         return res
 sol = Solution()
-print(sol.smallerNumbersThanCurrent(nums1)) #[4, 0, 1, 1, 3]
-print(sol.smallerNumbersThanCurrent(nums2)) #[2, 1, 0, 3]
+# print(sol.smallerNumbersThanCurrent(nums1)) #[4, 0, 1, 1, 3]
+# print(sol.smallerNumbersThanCurrent(nums2)) #[2, 1, 0, 3]
 
+nums1 = [8,1,2,2,3]
+nums2 = [6,5,4,8]
+nums3 = [4, 4, 8, 10, 11, 23, 8, 0, 1, 3, 101, 99]
+class Solution:
+    def smallerNumbersThanCurrent(self, nums):
+        temp_nums = sorted((nums))
+        nums_map = {}
+        for i, num in enumerate(temp_nums):
+            if num not in nums_map:
+                nums_map[num] = i
+        res = []
+        for num in nums:
+            res.append(nums_map[num])
+        return res
+sol = Solution()
+print(sol.smallerNumbersThanCurrent(nums1)) 
+#[4, 0, 1, 1, 3]
+print(sol.smallerNumbersThanCurrent(nums2)) 
+#[2, 1, 0, 3]
+print(sol.smallerNumbersThanCurrent(nums3)) 
+#[3, 3, 5, 7, 8, 9, 5, 0, 1, 2, 11, 10]
 
 
 
