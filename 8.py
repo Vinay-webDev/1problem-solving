6cddafd8fe7d01789911ec1efdc2321b21f21672
Vinay-webDev <justchatting3000@gmail.com>
2024-12-26 15:24:54 +0530

    #8

--- 8.py
+++ 8.py
@@ -3,16 +3,21 @@ prices1 = [7,1,5,3,6,4]
 prices2 = [7,6,4,3,1]
 class Solution:
     def maxProfit(self, prices):
-        l = 0
-        r = len(prices) - 1
+        l, r = 0, 1
         max_profit = 0
-        while l < r:
-            current_max_profit = abs(prices[l] - prices[r])
-            max_profit = max(max_profit, current_max_profit)
+        while r != len(prices):
             if prices[l] < prices[r]:
-                l = l + 1
+                current_max_profit = prices[r] - prices[l]
+                max_profit = max(max_profit, current_max_profit)
             else:
-                r = r - 1
+                l = r
+            r = r + 1
         return max_profit
 sol = Solution()
-print(sol.maxProfit(prices1))  #6
+print(sol.maxProfit(prices1))  #5
+print(sol.maxProfit(prices2))  #0
+
+
+
+
+
