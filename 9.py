f53619970415d128bd7dd30c6745289b3ba156c8
Vinay-webDev <justchatting3000@gmail.com>
2024-12-28 11:39:54 +0530

    #9✅

--- 9.py
+++ 9.py
@@ -11,11 +11,7 @@ sol = Solution()
 # print(sol.sortedSquares(nums2)) #[4, 9, 9, 49, 121]
 """above is a nub solution"""
 #split and merge
-nums1 = [-4,-1,0,3,10]
-nums2 = [-7,-3,2,3,11]
-nums3 = []
-nums4 = [1, 2, 3, 4]
-nums5 = [-5,-3,-2,-1]
+
 class Solution:
     def sortedSquares(self, nums):
         #edge cases
@@ -48,13 +44,36 @@ class Solution:
             r = r + 1
         return [n ** 2 for n in res]
 sol = Solution()
+# print(sol.sortedSquares(nums1)) #[0, 1, 9, 16, 100]
+# print(sol.sortedSquares(nums2)) #[4, 9, 9, 49, 121]
+# print(sol.sortedSquares(nums3)) #[]
+# print(sol.sortedSquares(nums4)) #[1, 4, 9, 16]
+# print(sol.sortedSquares(nums5)) #[25, 9, 4, 1]  wrong answer
+#❌❌❌Wrong Answer❌❌❌
+
+nums1, nums2 = [-4,-1,0,3,10], [-7,-3,2,3,11] 
+nums3, nums4 = [], [1, 2, 3, 4]
+nums5 = [-5,-3,-2,-1]
+class Solution:
+    def sortedSquares(self, nums):
+        if not nums:
+            return nums
+        l, r = 0, len(nums) - 1
+        res = []
+        while l <= r:
+            l_square = nums[l] ** 2
+            r_square = nums[r] ** 2
+            if l_square > r_square:
+                res.append(l_square)
+                l += 1
+            else:
+                res.append(r_square)
+                r -= 1
+        return res[::-1]
+sol = Solution()
 print(sol.sortedSquares(nums1)) #[0, 1, 9, 16, 100]
 print(sol.sortedSquares(nums2)) #[4, 9, 9, 49, 121]
 print(sol.sortedSquares(nums3)) #[]
 print(sol.sortedSquares(nums4)) #[1, 4, 9, 16]
-print(sol.sortedSquares(nums5)) #[25, 9, 4, 1]  wrong answer
-#❌❌❌Wrong Answer❌❌❌
-
-
-
+print(sol.sortedSquares(nums5)) #[1, 4, 9, 25]
 
